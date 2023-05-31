cef2d9858a2d314bb8884db36c849558df458803
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2023-05-31 23:18:03 +0530

    feat : Carousel

--- src/App.js
+++ src/App.js
@@ -1,8 +1,53 @@
-import './App.css';
+import "./App.css";
+import Carousel from "./components/Carousel";
+
 
 function App() {
+  const images = [
+    {
+      id: 1,
+      src: "https://www.isu.edu/media/top-level/terminal-four/tutorial/Davis-Field-Renovation-2020.jpg",
+      alt: "Image 1",
+      title: "Artist rendering",
+      description: "Artist rendering of completed Davis Field renovations",
+    },
+    {
+      id: 2,
+      src: "https://www.isu.edu/media/top-level/terminal-four/tutorial/200220-welcome-center-grand-opn-21.jpg",
+      alt: "Center grand opening",
+      title: "Center grand opening",
+      description:
+        "Ribbon cutting at the Student Welcome Center grand opening. President Satterlee and Tori Parks cut the ribbon at the Student Welcome Center opening",
+    },
+    {
+      id: 3,
+      src: "https://www.isu.edu/media/top-level/terminal-four/140828_GlowBowling_06.jpg",
+      alt: "Glow Bowling",
+      title: "Glow Bowling",
+      description:
+        "Glow Bowling at the Student Union Students participate in Glow Bowling during New Student Orientation",
+    },
+    {
+      id: 4,
+      src: "https://www.isu.edu/media/top-level/terminal-four/tutorial/100-Years-Web.png",
+      alt: "100 Years of Pharmacy",
+      title: "100 Years of Pharmacy",
+      description:
+        "Reflections: 100 Years of Pharmacy. Reflections: 100 Years of Pharmacy, a pharmacy student receives her white coat",
+    },
+  ];
   return (
-    <div className="App">
+    <div style={{ textAlign: "center" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          padding: "4px",
+        }}
+      >
+        <Carousel images={images} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Carousel.jsx
@@ -0,0 +1,199 @@
+import React, { useState, useEffect, useRef } from "react";
+// import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+
+
+const Carousel = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isSlideshowActive, setIsSlideshowActive] = useState(false);
+  const [sliderData, setSliderData] = useState(images[0]);
+  const [isImageClicked, setIsImageClicked] = useState(false);
+  const timeoutRef = useRef(null);
+
+  useEffect(() => {
+    if (isSlideshowActive) {
+      timeoutRef.current = setTimeout(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 3000);
+    }
+
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, [isSlideshowActive, images]);
+
+  const previousImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+    setIsSlideshowActive(false);
+    setIsImageClicked(false);
+  };
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setIsSlideshowActive(false);
+    setIsImageClicked(false);
+  };
+
+  const toggleSlideshow = () => {
+    setIsSlideshowActive((prevState) => !prevState);
+    setIsImageClicked(false);
+  };
+
+  const handleClick = (index) => {
+    setIsImageClicked(true);
+    setCurrentImageIndex(index);
+    const slider = images[index];
+    setSliderData(slider);
+    setIsSlideshowActive(false);
+  };
+
+  return (
+    <div>
+    <div
+      style={{
+        display: "flex",
+        marginBottom: "10px"
+      }}
+    >
+      <div
+        style={{
+          width: "50%",
+          maxHeight: "50%",
+          display: "flex",
+          flexDirection: "column",
+          objectFit: "cover",
+          border: "1px solid #ddd",
+          borderRadius: "5px"
+        }}
+      >
+        <CardMedia
+          component="img"
+          src={
+            isImageClicked ? sliderData.src : images[currentImageIndex].src
+          }
+          alt={
+            isImageClicked ? sliderData.alt : images[currentImageIndex].alt
+          }
+        />
+      </div>
+
+      <div
+        style={{
+          marginLeft: "10px",
+          width: "50%",
+          display: "flex",
+          flexDirection: "column",
+          marginTop: "10px",
+          textAlign: "center"
+        }}
+      >
+        <Typography variant="h6">
+          {isImageClicked
+            ? sliderData.title
+            : images[currentImageIndex].title}
+        </Typography>
+        <Typography variant="body1">
+          {isImageClicked
+            ? sliderData.description
+            : images[currentImageIndex].description}
+        </Typography>
+      </div>
+    </div>
+
+    <div
+      style={{
+        display: "flex",
+        overflowX: "scroll",
+        flex: "0 0 auto",
+        justifyContent: "center"
+      }}
+    >
+      {images.map((image, i) => (
+        <Card
+          key={image.id}
+          style={{
+            borderRadius: "5px",
+            maxHeight: "200px",
+            margin: "0 5px",
+            gap: "10px",
+            filter: i === currentImageIndex ? "none" : "grayscale(100%)"
+          }}
+          onClick={() => handleClick(i)}
+        >
+          <CardMedia component="img" src={image.src} alt={image.alt} />
+        </Card>
+      ))}
+    </div>
+
+    <div
+      style={{
+        marginTop: "10px",
+        display: "flex",
+        justifyContent: "center"
+      }}
+    >
+      <Button
+        onClick={previousImage}
+        style={{
+          margin: "0 5px",
+          padding: "4px 8px",
+          borderRadius: "5px",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          fontSize: "14px",
+          cursor: "pointer",
+          "&:hover": {
+            backgroundColor: "green"
+          }
+        }}
+      >
+        Previous
+      </Button>
+      <Button
+        onClick={toggleSlideshow}
+        style={{
+          margin: "0 5px",
+          padding: "4px 8px",
+          borderRadius: "5px",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          fontSize: "14px",
+          cursor: "pointer",
+          "&:hover": {
+            backgroundColor: "green"
+          }
+        }}
+      >
+        {isSlideshowActive ? "Pause" : "Play"}
+      </Button>
+      <Button
+        onClick={nextImage}
+        style={{
+          margin: "0 5px",
+          padding: "4px 8px",
+          borderRadius: "5px",
+          backgroundColor: "#007bff",
+          color: "#fff",
+          fontSize: "14px",
+          cursor: "pointer",
+          "&:hover": {
+            backgroundColor: "green"
+          }
+        }}
+      >
+        Next
+      </Button>
+    </div>
+  </div>
+  );
+};
+
+export default Carousel;
